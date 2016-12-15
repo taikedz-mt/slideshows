68307c92d4c2e080f5bfd0a873704bd54d4ba692
Tai @ Flex <dch.tai@gmail.com>
2016-12-15 21:57:06 +0000

    enforce JPEG usage

--- init.lua
+++ init.lua
@@ -4,10 +4,10 @@ local slides = {}
 local function showpic(imagename)
 	local fsn = "formspeccer:image"
 	formspeccer:clear(fsn)
-	formspeccer:newform(fsn,"10,7",{
+	formspeccer:newform(fsn,"4,3",{
 		background = {
 			xy = "0,0",
-			wh = "10,10",
+			wh = "4,3",
 			texture = imagename,
 		},
 	})
@@ -23,6 +23,14 @@ local function run_slideshow(deck)
 end
 
 function slideshow.register_slide(deckname,imagename)
+	if not imagename:match(".jpg$") and not imagename:match(".jpeg$") then
+		-- all media needs to be loaded over the wire to each client
+		-- reduce load time and bandwidth usage by making sure images are less than 64KB
+		-- using JPEG is a start
+		minetest.log("error","Use only properly compressed JPEG images with slideshows in 4:3 ratio. Reduce network traffic!")
+		return
+	end
+
 	if not slide[deckname] then
 		slides[deckname] = {}
 	end
